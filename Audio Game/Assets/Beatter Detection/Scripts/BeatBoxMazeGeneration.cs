7a3d694d16341ef56d19ae3a479dee3c4baa7d3e
rhiknow55 <ohryan55@gmail.com>
2017-04-26 22:42:43 -0700

    Indicator
    - Completed IndicatorScript
    - Two rows of indicator balls falling down to the static cylinder that
    indicates when to hit the beat

--- Audio Game/Assets/Beatter Detection/Scripts/BeatBoxMazeGeneration.cs
+++ Audio Game/Assets/Beatter Detection/Scripts/BeatBoxMazeGeneration.cs
@@ -9,33 +9,51 @@ public class BeatBoxMazeGeneration : MonoBehaviour {
 
 	// Public Variables
 	public float clutterSpikeInterval, spawnInterval;
+	public Transform rightSpawn, leftSpawn, rightEnd, leftEnd;
+	public GameObject indicatorPrefab;
+	public float timeItTakesForIndicatorToReachPlayer;
 
 	// Private Variables
 	List<int> listOfSpikes;
 	float changedTimeObstacle, changedTimeEnemy;
 	int numberOfSpikesInInterval;
 
+	List<GameObject> indicatorPool, obstaclePool, enemyPool;
+
 	//-----------------------------------------
 
 	void Start() {
-		listOfSpikes = new List<int>();		
+		listOfSpikes = new List<int>();
+
+		indicatorPool = new List<GameObject>();
+		Init(indicatorPool, indicatorPrefab, 30, "indicator");
+	}
+
+	void Init(List<GameObject> pool, GameObject prefab, int size, string name) {
+		for (int i = 0; i < size; i++) {
+			GameObject obj = Instantiate(prefab);
+			obj.SetActive(false);
+			obj.name = name + i;
+			pool.Add(obj);
+		}
 	}
 
 	void Update() {
 		listOfSpikes = GameObject.Find("Audio Compiler").GetComponent<AudioCompiler>().CheckForFreqSpike();
 
-		if (listOfSpikes.Contains(1) && Time.time >= changedTimeObstacle + spawnInterval) DoBass();
-		if (listOfSpikes.Count > 0 && !listOfSpikes.Contains(1) && Time.time >= changedTimeEnemy + spawnInterval) DoRest();
+		if (listOfSpikes.Contains(1) && Time.time >= changedTimeObstacle + spawnInterval) GenerateObstacles();
+		if (listOfSpikes.Count > 0 && !listOfSpikes.Contains(1) && Time.time >= changedTimeEnemy + spawnInterval) GenerateEnemies();
 	}
 
 	
-	void DoBass() {
-		DoBassMore();
+	void GenerateObstacles() {
+		SpawnObstacles();
 		changedTimeObstacle = ResetPoolToSpawn();
+		changedTimeEnemy = ResetPoolToSpawn();
 	}
 
-	void DoRest() {
-		DoRestMore();
+	void GenerateEnemies() {
+		SpawnEnemies();
 		changedTimeEnemy = ResetPoolToSpawn();
 	}
 
@@ -44,11 +62,37 @@ public class BeatBoxMazeGeneration : MonoBehaviour {
 	}
 
 	// Randomize obstacle spawn? have a system? pattern?
-	void DoBassMore() {
+	void SpawnObstacles() {
 		print("Obstacle spawned");
+
+		GameObject obj = indicatorPool[0];
+		indicatorPool.RemoveAt(0);
+
+		if(obj.GetComponent<IndicatorScript>() == null) obj.AddComponent<IndicatorScript>();
+		obj.GetComponent<IndicatorScript>().SetEndPos(leftEnd.position);
+		obj.GetComponent<IndicatorScript>().SetStartPos(leftSpawn.position);
+		obj.GetComponent<IndicatorScript>().SetTimeItTakes(timeItTakesForIndicatorToReachPlayer);
+		obj.GetComponent<IndicatorScript>().ResetIndicator();
+
+		obj.SetActive(true);
 	}
 
-	void DoRestMore() {
+	void SpawnEnemies() {
 		print("Enemy spawned");
+
+		GameObject obj = indicatorPool[0];
+		indicatorPool.RemoveAt(0);
+
+		if (obj.GetComponent<IndicatorScript>() == null) obj.AddComponent<IndicatorScript>();
+		obj.GetComponent<IndicatorScript>().SetEndPos(rightEnd.position);
+		obj.GetComponent<IndicatorScript>().SetStartPos(rightSpawn.position);
+		obj.GetComponent<IndicatorScript>().SetTimeItTakes(timeItTakesForIndicatorToReachPlayer);
+		obj.GetComponent<IndicatorScript>().ResetIndicator();
+
+		obj.SetActive(true);
+	}
+
+	public void ReturnIndicatorToPool(GameObject i) {
+		indicatorPool.Add(i);
 	}
 }
---
+++ Audio Game/Assets/Beatter Detection/Scripts/IndicatorScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorScript : MonoBehaviour {
+
+	// Public Variables
+
+	// Private Variables
+	Vector3 startPos, endPos;
+	float traversalTime;
+	float timeItTakesToReachEndPos;
+	float startTime;
+	float leewayTime = 0.5f;
+
+	void Update() {
+		// leewayTime is for the player to be able to hit the beat for some time AFTER the actual beat - in case the player reacts late
+		if (this.transform.position == endPos && TimePassed() > timeItTakesToReachEndPos + leewayTime) {
+			BackToPool();
+		}else {
+			traversalTime = TimePassed() / timeItTakesToReachEndPos;
+			PosLerp();
+		}
+	}
+
+	public float TimePassed() {
+		float currentTime = Time.time;
+		return currentTime - startTime;
+	}
+
+	public void PosLerp() {
+		this.gameObject.transform.position = Vector3.Lerp(startPos, endPos, traversalTime);
+	}
+
+	public void SetStartPos(Vector3 s) {
+		startPos = s;
+		this.gameObject.transform.position = startPos;
+	}
+
+	public void SetEndPos(Vector3 s) {
+		endPos = s;
+	}
+
+	public void SetTimeItTakes(float t) {
+		timeItTakesToReachEndPos = t;
+	}
+
+	public void ResetIndicator() {
+		startTime = Time.time;
+	}
+
+	void BackToPool() {
+		this.gameObject.SetActive(false);
+		GameObject.Find("BeatBoxMazeGenerator").GetComponent<BeatBoxMazeGeneration>().ReturnIndicatorToPool(this.gameObject);
+	}
+}
